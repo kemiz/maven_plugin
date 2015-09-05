03ac4622f511701ea22bc264aeb6accbc618353c
kemiz <kemimeister@gmail.com>
2015-09-05 17:05:44 +0100

    fixed download operation

--- maven_plugin/tasks.py
+++ maven_plugin/tasks.py
@@ -7,18 +7,18 @@ import tempfile
 from package_installer_plugin.utils import run, unzip, download_file
 
 
-def package(module_source_zip_path, app_name, **_):
-    """
-    Downloads application source as zip and uses maven to
-    build and package a deployable WAR file
-    """
-
-    unzip(module_source_zip_path, '/tmp')
-    _run_maven_command('/tmp/{0}/pom.xml'.format(app_name), MVN_PACKAGE)
-    return '/tmp/{0}/target/{0}.war'.format(app_name)
-
-
-def _run_maven_command(pom_xml, mvn_operation):
-    package_command = 'mvn -f {0} {1}'.format(pom_xml, mvn_operation)
-    # ctx.logger.info('Executing maven operation: ' + package_command)
-    run(package_command)
+# def package(module_source_zip_path, app_name, **_):
+#     """
+#     Downloads application source as zip and uses maven to
+#     build and package a deployable WAR file
+#     """
+#
+#     unzip(module_source_zip_path, '/tmp')
+#     _run_maven_command('/tmp/{0}/pom.xml'.format(app_name), MVN_PACKAGE)
+#     return '/tmp/{0}/target/{0}.war'.format(app_name)
+#
+#
+# def _run_maven_command(pom_xml, mvn_operation):
+#     package_command = 'mvn -f {0} {1}'.format(pom_xml, mvn_operation)
+#     # ctx.logger.info('Executing maven operation: ' + package_command)
+#     run(package_command)
